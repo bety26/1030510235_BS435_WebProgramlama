44a97451253baa2d17d4518932ed59885dec2e4c
betül yılmaz <byilmas4@gmail.com>
2023-11-13 14:18:08 +0300

    2

--- src/components/Anasayfa.jsx
+++ src/components/Anasayfa.jsx
@@ -9,7 +9,7 @@ function Anasayfa(){
                 <p>
                     Aww yeah, you successfully read this important alert message. This
                     example text is going to run a bit longer so that you can see how
-                    spacing within an alert works with this kind of content.
+                    spacing within an alert works with this kind of content...
                 </p>
                 <hr />
                 <p className="mb-0">
