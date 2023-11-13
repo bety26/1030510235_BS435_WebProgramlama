41ac33acacbe22694fec4577bf0c841a9e819039
bety26 <99122148+bety26@users.noreply.github.com>
2023-11-13 13:08:40 +0300

    2

---
+++ src/components/Oyun1.jsx
@@ -0,0 +1,10 @@
+import React  from "react";
+import {Alert} from "react-bootstrap";
+function Oyun1(){
+    return(
+        <Alert variant="warning" className="mt-3">
+            Oyun 1 sayfası
+        </Alert>
+    )
+}
+export  default Oyun1
---
+++ src/components/Oyun2.jsx
@@ -0,0 +1,10 @@
+import React  from "react";
+import {Alert} from "react-bootstrap";
+function Oyun2(){
+    return(
+        <Alert variant="warning" className="mt-3">
+            Oyun 2 sayfası
+        </Alert>
+    )
+}
+export  default Oyun2
