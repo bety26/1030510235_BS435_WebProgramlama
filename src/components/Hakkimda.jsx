a7954043a3f896bcc6c04f3869a3f1c2b831816b
bety26 <99122148+bety26@users.noreply.github.com>
2023-12-28 14:12:02 +0300

    Hakkımda sayfası düzenlendi

--- src/components/Hakkimda.jsx
+++ src/components/Hakkimda.jsx
@@ -1,10 +1,21 @@
-import React  from "react";
-import {Alert} from "react-bootstrap";
-function Hakkimda(){
-    return(
-        <Alert variant="warning" className="mt-3">
-            Hakkımda sayfası
-        </Alert>
-    )
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+function Hakkimda() {
+    return (
+        <Container>
+            <Row>
+                <Col xs={3} md={2} className="float-right">
+                    <img src="./images/1559676034387.png" style={{ width: '200px', height: '250px' }} alt="Profil Resmi" />
+                </Col>
+                <Col xs={9} >
+                    <h1>Betül Gül Yılmaz</h1>
+                    <h3>1030510235</h3>
+                    <h5>Bilgisayar Mühendisliği</h5>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
-export  default Hakkimda
+
+export default Hakkimda;
